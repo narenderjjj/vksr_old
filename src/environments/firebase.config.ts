156bb87c07734853d0da6e2628ad6c67c57184e9
narenderjjj <narenderjjj@gmail.com>
2017-12-28 08:52:07 -0500

    Add files via upload

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-l9HGhrP4Pfs20EbJUT8k3detzqt3egk",
+    authDomain: "vksr-c4c54.firebaseapp.com",
+    databaseURL: "https://vksr-c4c54.firebaseio.com",
+    storageBucket: "vksr-c4c54.appspot.com",
+    messagingSenderId: "565743729751"
+};
